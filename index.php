b5d1624b23aa4673a509b08c3f24bae169a430c8
Droppledev <ayasfn@gmail.com>
2019-05-03 00:24:37 +0700

    add todo

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 $host = "droppledev.database.windows.net";
 $user = "droppledev";
 $pass = "testAzure19";
@@ -118,7 +117,3 @@ if (isset($_POST['add'])) {
 </body>
 
 </html>
-=======
-echo 'hello todo azure app';
-?>
->>>>>>> 192c67c80a83b4975fa68fb97ab9445bb955701f
